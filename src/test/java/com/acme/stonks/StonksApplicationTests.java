a2d7ac4aed52a82e1908e212c43d36220172ab7f
erick <erickquiber@gmail.com>
2021-05-20 01:17:26 -0500

    new 1

---
+++ src/test/java/com/acme/stonks/StonksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.stonks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
