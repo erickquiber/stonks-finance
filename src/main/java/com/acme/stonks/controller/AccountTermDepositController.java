c165e660b7c8988100f7cbff74c2bf602b0e99e5
erick <erickquiber@gmail.com>
2021-06-01 00:10:42 -0500

    new changes tags2

--- src/main/java/com/acme/stonks/controller/AccountTermDepositController.java
+++ src/main/java/com/acme/stonks/controller/AccountTermDepositController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-@Tag(name="Cuenta Plazo Fijo",description = "API")
+@Tag(name="Account Term Deposit",description = "API")
 @RestController
 @RequestMapping("/api")
 public class AccountTermDepositController {
