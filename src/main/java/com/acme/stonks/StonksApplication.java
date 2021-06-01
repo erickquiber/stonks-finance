e6d5da6846b7d26611c91ded3d65783fb6b4252f
erick <erickquiber@gmail.com>
2021-05-31 21:19:20 -0500

    new changes CORS

--- src/main/java/com/acme/stonks/StonksApplication.java
+++ src/main/java/com/acme/stonks/StonksApplication.java
@@ -1,9 +1,14 @@
 package com.acme.stonks;
 
+
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class StonksApplication {
@@ -12,10 +17,24 @@ public class StonksApplication {
 
         SpringApplication.run(StonksApplication.class, args); }
 
+
         @Bean
         public ModelMapper modelMapper() {
             return new ModelMapper();
         }
+
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("/**")
+                        .allowedMethods("*").allowedHeaders("*");
+            }
+        };
+    }
 
 
 
